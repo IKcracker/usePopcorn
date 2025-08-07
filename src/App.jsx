f57b970a8424f77fa29b687f2987f48b49f90720
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2025-08-07 14:55:41 +0200

    breake down a huge component into reusable componets

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import NavBar from "./components/navbar";
+import ToggleButton from "./components/toggle-btn";
+import UnwatchedCard from "./components/unwatched-card";
+import Summary from "./components/summary";
+import WatchedCard from "./components/watched-card";
+import { tempMovieData, tempWatchedData } from "./lib/data";
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen1, setIsOpen1] = useState(true);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  return (
+    <>
+      <NavBar query={query} setQuery={setQuery} movies={movies} />
+      <main className="main">
+        <div className="box">
+          <ToggleButton setIsOpen={setIsOpen1} isOpen={isOpen1} />
+          {isOpen1 && (
+            <ul className="list">
+              {movies?.map((movie) => (
+                <UnwatchedCard movie={movie} />
+              ))}
+            </ul>
+          )}
+        </div>
+
+        <div className="box">
+          <ToggleButton setIsOpen={setIsOpen2} isOpen={isOpen2} />
+          <Summary watched={watched} />
+          {isOpen2 && (
+            <ul className="list">
+              {watched.map((movie) => (
+                <WatchedCard movie={movie} />
+              ))}
+            </ul>
+          )}
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/components/toggle-btn.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ToggleButton({ setIsOpen, isOpen }) {
+  return (
+    <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+      {isOpen ? "–" : "+"}
+    </button>
+  );
+}
